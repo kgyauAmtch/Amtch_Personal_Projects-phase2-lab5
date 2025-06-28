665b02172344874673e27070246731065bef5b1f
kgyauAmtch <kwame.ofori-gyau@amalitechtraining.org>
2025-06-28 10:44:33 +0000

    added an init script to treat directory as a package

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+"""
+Tests package for Lab 5 ETL Pipeline
+
+This package contains unit tests for:
+- Raw data processing jobs (test_raw_jobs.py)
+- Staging data processing jobs (test_staging_jobs.py)
+- Shared test configuration and fixtures (conftest.py)
+"""
+
+__version__ = "1.0.0"
+__author__ = "KOG"
